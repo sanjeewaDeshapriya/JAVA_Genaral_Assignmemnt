53b81a08400f7c7511e480d95304eca1b4cc1dd7
Dilshan Sanjeewa Deshapriya <134911483+sanjeewaDeshapriya@users.noreply.github.com>
2023-08-06 17:33:24 +0530

    Q1 Code Uploaded

---
+++ q1/q1/src/App.java
@@ -0,0 +1,10 @@
+class Name{
+    public Name(String name){
+        System.out.println(name);
+    }
+}
+public class App {
+    public static void main(String[] args) throws Exception {
+        Name name1= new Name("Sanjeewa");
+    }
+}
